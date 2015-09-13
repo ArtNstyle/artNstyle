8103f8a4c2aa2a0df6aa100f41a8a68a9f0dc94f
tkarling <tkarling@live.com>
2015-09-12 19:44:22 -0600

    user can view initial static version of stylists page

--- src/features/stylists/index.js
+++ src/features/stylists/index.js
@@ -4,9 +4,10 @@ import angular from 'angular';
 require('angular-material');
 import ngMaterial from 'angular-material';
 
+import stylist from './stylist.directive';
 import StylistsController from './stylists.controller';
 
-export default angular.module('app.stylists', [ngMaterial])
+export default angular.module('app.stylists', [ngMaterial, stylist])
     .controller('StylistsController', StylistsController)
     .name;
 
