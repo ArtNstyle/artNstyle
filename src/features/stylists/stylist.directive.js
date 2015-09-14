d14e4fa30d889e11d56141e1e96bc68b809df1ab
tkarling <tkarling@live.com>
2015-09-14 10:57:37 -0600

    reading stylists from the stylist controller now
    (compared to hardcoded data in directive)

--- src/features/stylists/stylist.directive.js
+++ src/features/stylists/stylist.directive.js
@@ -4,23 +4,17 @@ function stylist() {
     return {
         restrict: 'E',
         scope: {
-            stylist: '='
+            info: '='
         },
         template:
         '<div class="stylist">' +
-            '<div class="square" ng-style="{\'background-image\':\'url({{image}})\'}" ></div>' +
-            '<div class="name">{{name}}</div>' +
-            '<div class="title">{{title}}</div>' +
-            '<p class="bio">{{bio}}</p>' +
-            '<a ng-if="link" ng-href="{{link}}">click to shedule appointment</a>' +
+            '<div class="square" ng-style="{\'background-image\':\'url({{info.image}})\'}" ></div>' +
+            '<div class="name">{{info.name}}</div>' +
+            '<div class="title">{{info.title}}</div>' +
+            '<p class="bio">{{info.bio}}</p>' +
+            '<a ng-show="info.link" ng-href="{{info.link}}">click to shedule appointment</a>' +
         '</div>',
         controller: function($scope) {
-
-            $scope.name = "Jane Doe";
-            $scope.title = "Stylist";
-            $scope.bio = "Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes";
-            $scope.link = "https://www.google.com/";
-            $scope.image = "http://45.55.16.198:3039/api/pocketScrum/fullpic?id=55f4b2de516b4266230081c2";
         }
     }
 }
