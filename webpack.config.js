d19e0ce878dea9327ded5c4eec9487e966e3ef78
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2015-09-15 16:39:06 -0600

    included plugin extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: path.resolve('core/client'),
@@ -22,13 +23,13 @@ module.exports = {
         loaders: [
             {
                 test: /\.css$/,
-                //exclude: /node_modules/,
-                loader: 'style-loader!css-loader'
+                // exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
             },
             {
                 test: /\.scss$/,
-                exclude: /node_modules/,
-                loader: 'style-loader!css-loader!sass-loader'
+                // exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
             },
             {
                 test: /\.es6$/,
@@ -52,6 +53,10 @@ module.exports = {
             }
         ]
     },
+
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ],
 
     resolve: {
         extensions: ['', '.js', '.es6']
