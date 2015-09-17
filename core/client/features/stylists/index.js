41428961133eba5d59edc1047496561e56aa45a8
tkarling <tkarling@live.com>
2015-09-17 14:38:47 -0600

    user can edit stylist texts only after hitting edit button
    moved editable texts into directive
    some styling updates

--- core/client/features/stylists/index.js
+++ core/client/features/stylists/index.js
@@ -1,14 +1,15 @@
 import '../stylists/stylists.scss';
 
 import mainMenu from '../../directives/mainMenu/mainMenu.directive';
-import editable from '../../directives/editable.directive';
+import editableText from '../../directives/editableText.directive';
 import addItem from '../../directives/addItem.directive';
+
 import stylist from '../stylists/stylist.directive';
 
 import baseWeb from '../../services/baseWeb.service';
 
 import stylistsController from '../stylists/stylists.controller.es6';
 
-export default angular.module('app.stylists', [mainMenu, stylist, editable, addItem, baseWeb])
+export default angular.module('app.stylists', [mainMenu, stylist, editableText, addItem, baseWeb])
     .controller('stylistsController', stylistsController)
     .name;
