6802ea7cc746430b53cf6c4b168fb190e6197535
tkarling <tkarling@live.com>
2015-09-16 21:14:33 -0600

    user can see page specific bg color on navbar

--- core/client/features/salon/index.js
+++ core/client/features/salon/index.js
@@ -1,8 +1,10 @@
 import '../salon/salon.scss';
 
+import mainMenu from '../../directives/mainMenu/mainMenu.directive';
+
 import salonController from '../salon/salon.controller.es6';
 
-export default angular.module('app.salon', [])
+export default angular.module('app.salon', [mainMenu])
     .controller('salonController', salonController)
     .name;
 
