6274f1e94c9c520cbeb386e5303eac594a9c8114
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2015-09-18 13:16:51 -0600

    authenticates with google

--- core/server/config/express.js
+++ core/server/config/express.js
@@ -8,13 +8,32 @@ var config = require('./config'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     session = require('express-session'),
+    passport = require('passport'),
     webpack = require('webpack'),
-    webpackMiddleware = require('webpack-dev-middleware');
+    webpackMiddleware = require('webpack-dev-middleware'),
+    ejs = require('ejs'),
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 
 module.exports = function () {
     // generates the app object
     var app = express();
+
+
+    // here we set our templating engine
+    // route is relative to server.js
+    app.set('views', './core/server/views');
+    app.set('view engine', 'ejs');
+
+
+    // this middleware will run no matter the environment
+    app.use(cors());
+    app.use(bodyParser.urlencoded(
+        {
+            extended: true
+        }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
 
 
     // Environment-dependant middleware
@@ -31,16 +50,6 @@ module.exports = function () {
     }
 
 
-    // this middleware will run no matter the environment
-    app.use(cors());
-    app.use(bodyParser.urlencoded(
-        {
-            extended: true
-        }));
-    app.use(bodyParser.json());
-    app.use(methodOverride());
-
-
     // cookie support
     app.use(session({
         saveUninitialized: true,
@@ -48,11 +57,24 @@ module.exports = function () {
         secret: config.sessionSecret
     }));
 
-
-    // here we set our templating engine
-    // route is relative to server.js
-    app.set('views', './core/server/views');
-    app.set('view engine', 'ejs');
+    // google passport oauth
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use(new GoogleStrategy(
+        {
+            clientID: "517772876721-87ob4ks1iv2199rc0eiav6odb2jb6u7q.apps.googleusercontent.com",
+            clientSecret: "PFu1kBqYyBJGub-bl07R1v0E",
+            callbackURL: "http://localhost:5000/auth/google/callback"
+        },
+        function (req, accessToken, refreshToken, profile, done) {
+            done(null, profile);
+        }));
+    passport.serializeUser(function (user, done) {
+        done(null, user);
+    });
+    passport.deserializeUser(function (user, done) {
+        done(null, user);
+    });
 
 
     // HERE WE INCLUDE THE ROUTES
@@ -62,12 +84,13 @@ module.exports = function () {
     require('../routes/cart.server.routes.js')(app);
     require('../routes/event.server.routes.js')(app);
     require('../routes/stylists.server.routes.js')(app);
-
+    require('../routes/auth.server.routes.js')(app);
 
 
     // THIS WILL BE ANGULAR APP
     // needs to come after setting the rendering engine
     app.use(express.static('./core/client'));
+
 
     return app;
 };
---
+++ core/server/routes/auth.server.routes.js
@@ -0,0 +1,28 @@
+var usersCtrl = require('../controllers/auth.server.controller.js');
+var passport = require('passport');
+
+module.exports = function (app) {
+    app.route('/auth/google/callback')
+        .get(passport.authenticate('google', {
+            successRedirect: '/loggeduser',
+            failure: '/error/'
+        }));
+
+    app.route('/auth/google')
+        .get(passport.authenticate('google', {
+            scope: [
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/userinfo.email'
+            ]
+        }));
+
+    app.route('/loggeduser')
+        .get(function (req, res) {
+            res.render('loggeduser', {
+                user: {
+                    name: req.user.displayName,
+                    image: req.user._json.image.url
+                }
+            });
+        });
+};
