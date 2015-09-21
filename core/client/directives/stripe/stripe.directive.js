e517292ad10bff36c29c475d62912fb176a33bbf
Mackay Nicolson <mac.nicolson3@gmail.com>
2015-09-20 20:03:30 -0600

    update

---
+++ core/client/directives/stripe/stripe.directive.js
@@ -0,0 +1,15 @@
+
+function stripe() {
+    return {
+        restrict: 'E',
+        templateUrl: "../../directives/stripe/stripe.template.html",
+        link: function( $scope, lElem, lAttr ){
+        }
+    }
+}
+
+
+export default angular.module('directives.stripe', [])
+    .directive('stripe', stripe)
+    .name;
+
--- core/client/features/cart/index.js
+++ core/client/features/cart/index.js
@@ -2,9 +2,11 @@ import '../cart/cart.scss';
 
 import mainMenu from '../../directives/mainMenu/mainMenu.directive';
 
+import stripe from '../../directives/stripe/stripe.directive';
+
 import cartController from '../cart/cart.controller.es6';
 
-export default angular.module('app.cart', [mainMenu])
+export default angular.module('app.cart', [mainMenu, stripe])
     .controller('cartController', cartController)
     .name;
 
