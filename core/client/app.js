3e6646c095f74867e4d6e1d0852375c781d2e4a7
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2015-09-22 16:05:25 -0600

    created service

--- core/client/app.js
+++ core/client/app.js
@@ -28,26 +28,7 @@ import artists from './features/artists';
 import events from './features/events';
 
 
-var app = angular.module('app', [uiRouter, ngMaterial, goClick, home, stylists, salon, tanning, artists, events]);
-
-// Dear teammates: this controller would serve the only purpose of making the function $scope.isLoggedIn
-// globally available in all controllers, so we can use ng-show/ng-hide on elements that only
-// the owner should see
-// app.controller('checkAdminController', function ($scope, $http, $rootScope) {
-//     $scope.isLoggedIn = function () {
-//         $http.get('/checklogin')
-//             .success(function (data) {
-//                 console.log(data);
-//                 $rootScope.loggedIn = data;
-//             })
-//             .error(function (data) {
-//                 console.log('error: ' + data);
-//             });
-//     };
-// });
-
 var app = angular.module('app', [uiRouter, ngMaterial, ngFileUpload, goClick, home, stylists, salon, tanning, artists, events]);  //
-
 
 
 require('./routes')(app);
