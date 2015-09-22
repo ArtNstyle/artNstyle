017e6f229f28ca5f3eefec4df5b5b52d498337d9
Tami Wohlers <tamiwohelrs28@yahoo.com>
2015-09-22 15:03:34 -0600

    update to merge

--- core/client/features/cart/index.js
+++ core/client/features/cart/index.js
@@ -2,8 +2,7 @@ import '../cart/cart.scss';
 
 import mainMenu from '../../directives/mainMenu/mainMenu.directive';
 
-import cartController from './cart.controller.js';
-
+import cartController from '../cart/cart.controller.es6';
 export default angular.module('app.cart', [mainMenu])
     .controller('cartController',cartController)
     .name;
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+    devtool: "eval",
     context: path.resolve('core/client'),
 
     entry: './app.js',
