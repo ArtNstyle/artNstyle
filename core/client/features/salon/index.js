aff6ea239854f8046769ee09e4114111cf170412
Jessica <BrownJ0923@gmail.com>
2015-09-24 02:01:55 -0600

    updated versa spa page

--- core/client/features/salon/index.js
+++ core/client/features/salon/index.js
@@ -8,6 +8,6 @@ export default angular.module('app.salon', [mainMenu])
     .controller('salonController', salonController)
     .name;
 
-import '../salon/images/blacksign.jpg';
+
 import '../salon/images/flower.jpg';
 
