34864b0b526a88043ef13aa18006cf1712fd970f
Mackay Nicolson <mac.nicolson3@gmail.com>
2015-09-25 20:13:17 -0600

    update

--- core/server/models/stripe.server.model.js
+++ core/server/models/stripe.server.model.js
@@ -1,20 +1,9 @@
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-<<<<<<< HEAD
-// stripeSchema = new mongoose.Schema({
-//   token: {type: String, required: true},
-//   subscription: {
-//   	     type: String,
-//   		  enum: ['one-time', 
-//          'subscription']
-//   }
-// })
-=======
+
 stripeSchema = new mongoose.Schema({
   email: {type: String, required: true},
   token: {type: String, required: true}
 })
->>>>>>> dbe5e1812730c8f00aae9e01b27768d68fea24d4
-
-// module.exports = mongoose.model('stripe', stripeSchema)
+ module.exports = mongoose.model('stripe', stripeSchema)
 
