7e31c6a31deab3d63240486e0dba412741b031ab
Ladrillo <gabriel@cabrejas.net>
2015-09-29 20:10:50 -0600

    logout link, tickets only show if authenticated

--- core/client/features/contact/index.js
+++ core/client/features/contact/index.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     require('./contact.controller')(app);
     require('../../services/tickets.service.js')(app);
+    require('../../services/loggedUserES5.service.js')(app);
 };
 
