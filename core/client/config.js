3cd06964874f6fb6efd9da42dfda51bca795971a
Jessica <BrownJ0923@gmail.com>
2015-10-03 18:51:19 -0600

    update web pack and developer modes

--- core/client/config.js
+++ core/client/config.js
@@ -1,2 +1,2 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'; // change to development in order to work on localhost:5000
 module.exports = require('./env/' + process.env.NODE_ENV + '.js');
